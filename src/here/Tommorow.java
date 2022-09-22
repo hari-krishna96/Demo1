9ceae00d66b274840ffff1895a9de36a1c84f9cd
hari-krishna96 <jakkulaharikrishnayadav@gmail.com>
2022-09-22 23:59:35 +0530

    first commit

---
+++ src/here/Tommorow.java
@@ -0,0 +1,23 @@
+package here;
+
+import org.testng.annotations.Test;
+
+public class Tommorow {
+@Test
+	public void done()
+	{
+		System.out.println("king");
+	}
+
+
+
+	@Test
+	public void demo ()
+	{
+		System.out.println("queen");
+
+	}
+	
+	
+	
+}
